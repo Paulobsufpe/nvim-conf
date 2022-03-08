0761741e13f4367ef3d5de7ea95a7540e712c123
Paulobsufpe <paulo.bsimoes@ufpe.br>
2022-03-07 21:18:55 -0300

    change theme for lualine

--- lua/lualine-conf/init.lua
+++ lua/lualine-conf/init.lua
@@ -1,12 +1,10 @@
-if true then
-	require('lualine-conf/custom_bubbles_line')
-else
-	require'lualine'.setup({
-		options = {
-			theme = 'dracula',
-			-- section_separators = { left = '', right = ''},
-			-- component_separators = { left = '', right = ''}
-		}
-	})
-end
+-- require('lualine-conf/custom_bubbles_line')
+
+require'lualine'.setup({
+	options = {
+		theme = 'dracula',
+		-- section_separators = { left = '', right = ''},
+		-- component_separators = { left = '', right = ''}
+	}
+})
 
