3d24a31d9cc796ff7fee89326238d852939ce40b
Paulobsufpe <paulo.bsimoes@ufpe.br>
2022-06-15 23:00:00 -0300

    add TODO

--- init.lua
+++ init.lua
@@ -9,3 +9,4 @@ vim.cmd("autocmd BufRead,BufNewFile *.porth set filetype=porth")
 
 -- TODO: add callback for keybindings (ou o que?)
 -- TODO: disable nvim welcome
+-- TODO: reescrever a configuração de lsp (tomar de exemplo o github do nvim-lspconfig) ... lembrar de usar on_attach e criar bindings para `vim.diagnostic` também. (pesquisar para usar a mais nova API de keymaps)
