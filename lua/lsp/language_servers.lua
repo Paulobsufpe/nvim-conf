170a91ecd57e785b6aa36674c89802a174ebb56d
Paulobsufpe <paulo.bsimoes@ufpe.br>
2022-10-19 16:23:15 -0300

    fix lsp servers config

--- lua/lsp/language_servers.lua
+++ lua/lsp/language_servers.lua
@@ -2,7 +2,7 @@ local lsp_installer = require('nvim-lsp-installer')
 local lspconf = require('lspconfig')
 
 local capabilities = require('cmp_nvim_lsp')
-	.update_capabilities(
+	.default_capabilities(
 		vim.lsp.protocol.make_client_capabilities())
 
 lsp_installer.setup({ capabilities = capabilities })
@@ -11,10 +11,13 @@ lspconf.sumneko_lua.setup({
 	capabilities = capabilities,
 	settings = {
 		Lua = {
-			runtime = { version = 'LuaJIT', path = vim.split(package.path, ';') },
+			runtime = { 
+				version = 'LuaJIT', 
+				path = vim.split(package.path, ';') 
+			},
 			diagnostics = { globals = { 'vim' } },
 			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
+				library = vim.api.nvim_get_runtime_file('', true),
 				checkThirdParty = false
 			},
 			telemetry = { enable = false }
@@ -22,25 +25,37 @@ lspconf.sumneko_lua.setup({
 	}
 })
 
+lspconf.pylsp.setup({
+	capabilities = capabilities
+})
+
+lspconf.omnisharp.setup({
+	cmd = {
+		'dotnet', 
+		'/Users/paulobs/.local/share/nvim/lsp_servers\
+		/omnisharp/omnisharp/OmniSharp.dll'
+	},
+	capabilities = capabilities,
+	single_file_support = true
+})
+
 lspconf.jdtls.setup({
-	autostart = false,
 	capabilities = capabilities
 })
 
 lspconf.sourcekit.setup({
 	capabilities = capabilities,
-	filetypes = { "swift", "objective-c", "objective-cpp" },
+	filetypes = { 'swift', 'objective-c', 'objective-cpp' },
 	single_file_support = true
 })
 
 lspconf.clangd.setup({
 	capabilities = capabilities,
-	filetypes = { "c", "cpp" }
+	filetypes = { 'c', 'cpp' }
 })
 
 lspconf.rust_analyzer.setup({
 	cmd = { 'rustup', 'run', 'stable', 'rust-analyzer' },
-	autostart = false,
 	capabilities = capabilities
 })
 
@@ -49,7 +64,7 @@ lspconf.denols.setup({
 })
 
 lspconf.metals.setup({
-	capabilities = capabilities,
 	autostart = false,
+	capabilities = capabilities,
 	single_file_support = true
 })
