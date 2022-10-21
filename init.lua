e42b1c7ee4c68bb0d4dba15a8f87f2ca3ff0c596
Paulobsufpe <paulo.bsimoes@ufpe.br>
2022-10-21 14:54:29 -0300

    Update TODOs (add TODO for cmd '.')

--- init.lua
+++ init.lua
@@ -8,5 +8,7 @@ vim.cmd("autocmd BufRead,BufNewFile *.porth set filetype=porth")
 -- vim.cmd("autocmd BufReadPre *.asm let g:asmsyntax = \"fasm\"")
 
 -- TODO: add callback for keybindings (ou o que?)
--- TODO: disable nvim welcome
+-- TODO: disable nvim welcome (é possível?)
 -- TODO: reescrever a configuração de lsp (tomar de exemplo o github do nvim-lspconfig) ... lembrar de usar on_attach e criar bindings para `vim.diagnostic` também. (pesquisar para usar a mais nova API de keymaps)
+-- TODO: QuickFixCmdPre event binding for run :w before :make
+-- TODO: Avaliar mudar os atalhos para usar o cmd default  '.', ou remapear para outra tecla
