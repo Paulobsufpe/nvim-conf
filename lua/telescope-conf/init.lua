e0355c73bfabd2297edfb72bc9abd2a7dd05b64b
Paulobsufpe <paulo.bsimoes@ufpe.br>
2023-08-18 13:20:24 -0300

    [telescope] add multiple selection open file like fzf

--- lua/telescope-conf/init.lua
+++ lua/telescope-conf/init.lua
@@ -1,7 +1,6 @@
 local actions = require('telescope.actions')
 local previewers = require('telescope.previewers')
 
-
 -- Clone the default Telescope configuration
 local telescopeConfig = require('telescope.config')
 local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
@@ -13,8 +12,30 @@ table.insert(vimgrep_arguments, '--glob')
 table.insert(vimgrep_arguments, '!**/.git/*')
 -- table.insert(vimgrep_arguments, '--trim')
 
+local select_one_or_multi = function(prompt_bufnr)
+  local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+  if not vim.tbl_isempty(multi) then
+    require('telescope.actions').close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then
+        vim.cmd(string.format("%s %s", "edit", j.path))
+      end
+    end
+  else
+    require('telescope.actions').select_default(prompt_bufnr)
+  end
+end
+
+local mymaps = {
+	["<CR>"] = select_one_or_multi
+}
+
 require('telescope').setup {
 	defaults = {
+		mappings = {
+			i = mymaps, n = mymaps
+		},
 		layout_strategy = 'vertical',
 		prompt_prefix = ' ',
 		path_display = {},
