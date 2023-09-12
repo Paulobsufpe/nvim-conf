d56b7091d7b1c289d8da4ed68580512dd9ca5387
Paulobsufpe <paulo.bsimoes@ufpe.br>
2023-09-12 00:33:04 -0300

    quick and dirty hack to get autocmd for .fasm and .nasm

--- init.lua
+++ init.lua
@@ -5,6 +5,10 @@ require("plugins")
 
 -- vim.cmd("colorscheme dracula")
 vim.cmd("autocmd BufRead,BufNewFile *.porth set filetype=porth")
+-- TODO: refatorar isso, colocando os autocmd's em um lugar apropriado
+-- TODO: fazer limpa nos plugins. add cmp-commandline. ver sobre plugins canguru
+vim.cmd("autocmd BufRead,BufNewFile *.nasm set filetype=nasm")
+vim.cmd("autocmd BufRead,BufNewFile *.fasm set filetype=fasm")
 -- vim.cmd("autocmd BufReadPre *.asm let g:asmsyntax = \"fasm\"")
 
 -- TODO: considerar: nvim-surround, emet (?, html tags and etc.),
