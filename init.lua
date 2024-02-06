a3290bd4ebfa93207e630345e0c8be5957027d88
Paulo BS <paulo.bsimoes@ufpe.br>
2024-02-05 22:54:04 -0300

    basic setup for neovide

--- init.lua
+++ init.lua
@@ -18,3 +18,32 @@ vim.cmd("autocmd BufRead,BufNewFile *.fasm set filetype=fasm")
 -- TODO: disable nvim welcome (é possível?)
 -- TODO: QuickFixCmdPre event binding for run :w before :make
 -- TODO: Mudar find_files do Telescope para ignorar git o vcs`s
+
+if vim.g.neovide then
+	-- vim.g.neovide_font_features = {
+	-- 	["JetbrainsMono Nerd Font"] = {
+	-- 		"-liga",
+	-- 		"-clig",
+	-- 		"-dlig",
+	-- 		"-hlig",
+	-- 		"-rlig",
+	-- 	}
+	-- }
+	vim.o.guifont = "JetbrainsMono Nerd Font:h13"
+	-- vim.o.guifont = "Iosevka Nerd Font:h13"
+
+	vim.g.neovide_scale_factor = 1.0
+	local change_scale_factor = function(delta)
+		vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+	end
+	vim.keymap.set("n", "<C-=>", function()
+		change_scale_factor(1.25)
+		print("scale: " .. vim.g.neovide_scale_factor)
+	end)
+	vim.keymap.set("n", "<C-->", function()
+		change_scale_factor(1/1.25)
+		print("scale: " .. vim.g.neovide_scale_factor)
+	end)
+	vim.g.neovide_cursor_animate_in_insert_mode = false
+	vim.g.neovide_cursor_vfx_mode = "wireframe"
+end
