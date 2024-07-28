fb8c630b5dfef9a86e281bceceeb3eaa96b6249f
Paulobsufpe <paulo.bsimoes@ufpe.br>
2024-07-27 23:13:27 -0300

    [macos] use catppuccin frappe theme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -98,7 +98,7 @@ require("lazy").setup({
 		config = function()
 			require("catppuccin").setup {}
 			-- latte, frappe, macchiato, mocha
-			vim.g.catppuccin_flavour = "macchiato"
+			vim.g.catppuccin_flavour = "frappe"
 			vim.cmd [[colorscheme catppuccin]]
 		end
 	},
