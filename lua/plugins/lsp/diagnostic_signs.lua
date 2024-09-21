2e2584328e9344765de063718c03f8e62b7207b4
Paulobsufpe <paulo.bsimoes@ufpe.br>
2024-09-21 19:42:39 -0300

    typo

--- lua/plugins/lsp/diagnostic_signs.lua
+++ lua/plugins/lsp/diagnostic_signs.lua
@@ -1,6 +1,6 @@
-local signs = {Error = "E", Warn = "W", Hint = "H", Info = "I"}
+local signs = { Error = "E", Warn = "W", Hint = "H", Info = "I" }
 
 for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = ""})
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
