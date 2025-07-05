896165bf999f7bd81bcca322669ec7078342197a
Paulobsufpe <paulo.bsimoes@ufpe.br>
2025-07-05 03:05:54 -0300

    fix lsp capabilities

--- lua/plugins/lsp/language_servers.lua
+++ lua/plugins/lsp/language_servers.lua
@@ -1,8 +1,7 @@
 require('neoconf').setup {}
 local lspconf = require('lspconfig')
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp')
+local capabilities = require('cmp_nvim_lsp')
 		.default_capabilities(capabilities)
 
 -- Mappings
