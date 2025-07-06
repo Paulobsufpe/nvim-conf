3c3fc0233012024c37cc07a15dd8fc11ab30a92c
Paulobsufpe <paulo.bsimoes@ufpe.br>
2025-07-06 03:34:48 -0300

    [diagnostics] update settings

--- lua/plugins/lsp/diagnostic_signs.lua
+++ lua/plugins/lsp/diagnostic_signs.lua
@@ -1,6 +1,40 @@
-local signs = { Error = "E", Warn = "W", Hint = "H", Info = "I" }
+-- vim.diagnostic.config({
+-- 	signs = {
+-- 		text = {
+-- 			[vim.diagnostic.severity.ERROR] = 'E',
+-- 			[vim.diagnostic.severity.WARN]  = 'W',
+-- 			[vim.diagnostic.severity.HINT]  = 'H',
+-- 			[vim.diagnostic.severity.INFO]  = 'I',
+-- 		}
+-- 	},
+-- 	virtual_lines = true,
+-- 	severity_sort = true,
+-- 	float = true
+-- })
+vim.diagnostic.config {
+	severity_sort = true,
+	float = { border = 'rounded', source = 'if_many' },
+	underline = { severity = vim.diagnostic.severity.ERROR },
+	signs = vim.g.have_nerd_font and {
+		text = {
+			[vim.diagnostic.severity.ERROR] = '󰅚 ',
+			[vim.diagnostic.severity.WARN] = '󰀪 ',
+			[vim.diagnostic.severity.INFO] = '󰋽 ',
+			[vim.diagnostic.severity.HINT] = '󰌶 ',
+		},
+	} or {},
+	virtual_text = {
+		source = 'if_many',
+		spacing = 2,
+		format = function(diagnostic)
+			local diagnostic_message = {
+				[vim.diagnostic.severity.ERROR] = diagnostic.message,
+				[vim.diagnostic.severity.WARN] = diagnostic.message,
+				[vim.diagnostic.severity.INFO] = diagnostic.message,
+				[vim.diagnostic.severity.HINT] = diagnostic.message,
+			}
+			return diagnostic_message[diagnostic.severity]
+		end,
+	},
+}
 
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
